0cc525259be4179a7cfb5fbfdb6caef4a26a1bbe
Ivan C Myrvold <ivan@myrvold.org>
2022-10-21 15:45:45 +0200

    Initial Commit

---
+++ XComics/XComicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XComicsApp.swift
+//  XComics
+//
+//  Created by Ivan C Myrvold on 21/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct XComicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
